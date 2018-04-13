954f7bb2bb6557941053d0b5ba6a5042e8196eb7
Julien Schueller <schueller@phimeca.com>
2018-04-13 10:12:19 +0200

    Add manylinux1_x86_64

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 set -e
 echo TRAVIS_COMMIT=${TRAVIS_COMMIT}
 docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}
-tag="openturns/archlinux-mingw"
+tag="openturns/${IMAGE}"
 docker tag ${tag}:${TRAVIS_COMMIT} ${tag}:latest
 docker push ${tag}:latest
