8eff3f93f9c58a7f2fe4d086fa1050058ff2131a
Julien Schueller <schueller@phimeca.com>
2022-11-25 16:31:15 +0100

    debian-aarch64/apt_install_nopost.sh

---
+++ debian-aarch64/apt_install_nopost.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+cd /tmp
+for pkg in $@
+do
+  pkgname=`echo ${pkg} | cut -d ":" -f1`
+  arch=`echo ${pkg} | cut -d ":" -f2`
+  apt-get download ${pkgname}:${arch} python3-minimal:${arch}
+  dpkg --unpack ${pkgname}_*_${arch}.deb
+  rm -f /var/lib/dpkg/info/${pkgname}.postinst
+  dpkg --configure ${pkgname}:${arch}
+done
