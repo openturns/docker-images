b06fe2c60e2e762127f6ec72c1a48417c31aa85d
Julien Schueller <schueller@phimeca.com>
2022-11-28 08:39:30 +0100

    debian-aarch64: Set flags

--- debian-aarch64/aarch64-cmake.sh
+++ debian-aarch64/aarch64-cmake.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+# from dpkg-buildflags
+export CPPFLAGS="-Wdate-time -D_FORTIFY_SOURCE=2"
+export CXXFLAGS="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security ${CPPFLAGS}"
+export CFLAGS="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security ${CPPFLAGS}"
+export LDFLAGS="-Wl,-Bsymbolic-functions -Wl,-z,relro"
+
 cmake -DCMAKE_TOOLCHAIN_FILE=/usr/share/toolchain-aarch64.cmake -DCMAKE_BUILD_TYPE=None -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/usr/aarch64-linux-gnu "$@"
