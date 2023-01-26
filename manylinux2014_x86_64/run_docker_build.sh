fc752bda9619d76180e58114b4df1c143c2c1c22
Julien Schueller <schueller@phimeca.com>
2023-01-26 09:53:56 +0100

    manylinux: Python_EXECUTABLE

--- manylinux2014_x86_64/run_docker_build.sh
+++ manylinux2014_x86_64/run_docker_build.sh
@@ -8,9 +8,7 @@ cd openturns
 cmake \
   -DCMAKE_INSTALL_PREFIX=$PWD/install \
   -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
-  -DSWIG_COMPILE_FLAGS="-O0" -DCMAKE_CXX_FLAGS="-Wall -D_GLIBCXX_ASSERTIONS" \
-  -DPYTHON_INCLUDE_DIR=/opt/python/cp310-cp310/include/python3.10 \
-  -DPYTHON_LIBRARY=dummy \
-  -DPYTHON_EXECUTABLE=/opt/python/cp310-cp310/bin/python .
+  -DSWIG_COMPILE_FLAGS="-O0" -DCMAKE_CXX_FLAGS="-Wall -Wextra -D_GLIBCXX_ASSERTIONS" \
+  -DPython_EXECUTABLE=/opt/python/cp310-cp310/bin/python .
 make install
 ctest -R "Ipopt|Bonmin|NLopt|Ceres|Dlib|CMinpack|Study|Sample_csv|Sequence|Pagmo|HMatrix|KarhunenLoeveP1Algorithm|SymbolicFunction|Chi" -E cppcheck --output-on-failure --timeout 100 ${MAKEFLAGS}
