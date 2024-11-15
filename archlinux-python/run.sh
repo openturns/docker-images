3798249dee631a850906e48c9f23bb6454d003c6
Julien Schueller <schueller@phimeca.com>
2024-11-15 17:59:15 +0100

    nlopt

---
+++ archlinux-python/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -xe
+cd /tmp
+rm -rf nlopt
+git clone --depth 1 https://github.com/stevengj/nlopt.git
+cd nlopt
+cmake -DCMAKE_INSTALL_PREFIX=$PWD/install \
+      -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
+      -DCMAKE_C_FLAGS="--coverage" -DCMAKE_CXX_FLAGS="--coverage" \
+      -DSWIG_COMPILE_FLAGS="-O1" -DNLOPT_TESTS=ON -DNLOPT_GUILE=OFF -DNLOPT_OCTAVE=OFF .
+make install -j8
+ctest -R "memoize|python" --output-on-failure -j12 --repeat after-timeout:2 --schedule-random
+gcov `find src/ -name "*.gcno"`
+echo "gcov OK"
+lcov --capture --directory . --output-file coverage.info --include "*_wrap.cxx"
+echo "lcov OK"
+ulimit -Sv 100000
+genhtml --profile --output-directory coverage coverage.info
+echo "genhtml OK"
