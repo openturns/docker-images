fe847fd6df05bf28d6d5464fa610d667ef1225f6
Julien Schueller <schueller@phimeca.com>
2025-09-17 09:58:00 +0200

    manylinux: Add HiGHS solver

--- manylinux2014_x86_64/run_docker_build.sh
+++ manylinux2014_x86_64/run_docker_build.sh
@@ -7,8 +7,9 @@ git clone --depth 1 https://github.com/openturns/openturns.git
 cd openturns
 cmake \
   -DCMAKE_INSTALL_PREFIX=$PWD/install \
+  -DBLA_VENDOR=Generic \
   -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
   -DSWIG_COMPILE_FLAGS="-O0 -Wno-unused-parameter" -DCMAKE_CXX_FLAGS="-Wall -Wextra -D_GLIBCXX_ASSERTIONS" \
   -DPython_EXECUTABLE=/opt/python/cp310-cp310/bin/python .
 make install
-ctest -R "Ipopt|Bonmin|NLopt|Ceres|Dlib|CMinpack|Study|Sample_csv|Sequence|Pagmo|HMatrix|KarhunenLoeveP1Algorithm|SymbolicFunction|Chi" -E cppcheck --output-on-failure --timeout 100 ${MAKEFLAGS}
+ctest -R "Ipopt|Bonmin|NLopt|Ceres|Dlib|CMinpack|Study|Sample_csv|Sequence|Pagmo|HMatrix|KarhunenLoeveP1Algorithm|SymbolicFunction|Chi|HiGHS" -E cppcheck --output-on-failure --timeout 100 ${MAKEFLAGS}
